fbcc8981a184e2563eb6d99335ba4ebdc30aba38
seokhee516 <seokhee051@gmail.com>
2021-12-26 10:29:49 +0900

    commit

---
+++ 10809.py
@@ -0,0 +1,5 @@
+import sys
+word = sys.stdin.readline()
+base = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k','l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+for i in range(len(base)):
+    print(word.find(base[i]), end = ' ')
