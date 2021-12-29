c211ad9faa45ad8cc78efe7a56700175b50ddae1
seokhee516 <seokhee051@gmail.com>
2021-12-29 21:27:11 +0900

    commit

---
+++ 15650.py
@@ -0,0 +1,8 @@
+import itertools
+import sys
+N, M = map(int, sys.stdin.readline().split(' '))
+arr = list(range(1,N+1))
+for r in itertools.combinations(arr, M):
+    for i in range(len(r)):
+        print(r[i], end = ' ')
+    print('')
