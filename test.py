a065c613c3f10e9c250239a8f256e3a0b1d5a4e3
seokhee516 <seokhee051@gmail.com>
2022-01-31 10:49:13 +0900

    commit

--- test.py
+++ test.py
@@ -1,31 +1,55 @@
-input_str = '[(()]'
-input_str = list(input_str)
 
-stack = []
-for i in range(len(input_str)):
-    if input_str[i] =='(' or input_str[i] =='[':
-        stack.append(input_str[i])
+import sys
 
-    if input_str[i] ==')':
-        if len(stack) == 0:
-            stack.append(input_str[i])
-            break
-        if stack[-1] == '[':
-            break
-        elif stack[-1] == '(':
-            stack.pop()
+def heapify(tree, index, tree_size):
+    left_child_index = 2 * index
+    right_child_index = 2 * index + 1
+    
+    largest = index
 
-    if input_str[i] ==']':
-        if len(stack) == 0:
-            stack.append(input_str[i])
-            break
-        if stack[-1] == ')':
-            break
-        elif stack[-1] == '[':
-            stack.pop()
+    if 0 <left_child_index< tree_size and tree[left_child_index] > tree[index]:
+        largest = left_child_index
+    if 0 < right_child_index< tree_size and tree[right_child_index]>tree[index]:
+        largest = right_child_index
+    
+    if largest != index:
+        swap(tree, index, largest)
+        heapify(tree, largest, tree_size)
 
+def swap(tree, index1, index2):
+    tree[index1], tree[index2] = tree[index2], tree[index1]
 
-if len(stack) == 0:
-    print('yes')
-else:
-    print('no')
+def reverse_heapify(tree, index):
+    parent_index = index // 2
+    if 0 < parent_index < len(tree) and tree[parent_index]<tree[index]:
+        swap(tree, parent_index, index)
+        reverse_heapify(tree, parent_index)
+
+'''
+heap = [None]
+for _ in range(int(sys.stdin.readline().strip())):
+    x = int(sys.stdin.readline().strip())
+    if x == 0:
+        try:
+            swap(heap, 1, len(heap)-1)
+            return_value = heap.pop()
+            heapify(heap, 1, len(heap))
+            print(return_value)
+        except IndexError:
+            print(0)
+    else:
+        heap.append(x)
+        reverse_heapify(heap, len(heap)-1)
+'''
+
+heap = [None]
+for _ in range(int(sys.stdin.readline().strip())):
+    x = int(sys.stdin.readline().strip())
+    if x == 0:
+        if len(heap) == 1:
+            print(0)
+        else:
+            print(heap.pop(1))
+    else:
+        heap.append(x)
+        reverse_heapify(heap, len(heap)-1)
