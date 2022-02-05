314be900794447c3e0c08c7bd08a3be541ed4ed4
seokhee516 <seokhee051@gmail.com>
2022-02-05 23:59:05 +0900

    commit

--- test.py
+++ test.py
@@ -9,3 +9,39 @@ DFS 알고리즘
         옅은 회색 표시를 해준다
         스택에 넣는다.
 '''
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+graph = [
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+
+# visited = [False] * 9
+# dfs(graph, 1, visited)
+
+from collections import deque
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+visited = [False] * 9
+bfs(graph, 1, visited)
