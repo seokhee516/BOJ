e0d6e2c7e0383507e1e8324719a6f1ec12e422c9
정석희 <86893209+seokhee516@users.noreply.github.com>
2022-07-01 22:07:29 +0900

    [Bronze V] Title: 크냐?, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+while True:
+    n,m = map(int, input().split())
+    if n == 0 and m == 0:
+        break
+    if n > m:
+        print("Yes")
+    else:
+        print("No")
