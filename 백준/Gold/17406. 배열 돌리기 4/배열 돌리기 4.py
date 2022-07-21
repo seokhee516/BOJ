41e03cec04f9081dabac236284c0a004c685de01
정석희 <86893209+seokhee516@users.noreply.github.com>
2022-07-21 21:31:09 +0900

    Update 배열 돌리기 4.py

--- 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.py
+++ 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.py
@@ -1,5 +1,8 @@
 import sys
+from itertools import permutations
+import copy
 input = sys.stdin.readline
+# 시계 방향으로 회전
 def rotate(x1, y1, x2, y2, matrix):
     i = x1
     j = y1
@@ -23,28 +26,17 @@ def rotate(x1, y1, x2, y2, matrix):
 
 n, m, k= map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-# for _ in range(n):
-#     arr.append(list(map(int, input().split())))
+# 연산 리스트
 lst = [tuple(map(int, input().split())) for _ in range(k)]
 
-from itertools import permutations
-import copy
-# print(list(permutations(lst,k)))
 answer = 1e9
-# rotate(r-s-1, c-s-1, r+s-1, c+s-1, arr)
-permutations = list(permutations(lst,k))
+permutations = list(permutations(lst,k)) # 모든 연산 순열 확인
 for permutation in permutations:
-    arr_copy = copy.deepcopy(arr)
+    arr_copy = copy.deepcopy(arr) # 딥 카피로 돌려주기
     for p in permutation:
         r, c, s = p
-        # print(r-s-1, c-s-1, r+s-1, c+s-1)
-        for i in range((2*s+1)//2):
-            # print("i",i)
+        for i in range((2*s+1)//2): # 한칸씩 돌리기
             rotate(r-s-1+i, c-s-1+i, r+s-1-i, c+s-1-i, arr_copy)
-        # print(arr_copy)
-    # print("arr_copy",arr_copy)
-    # print()
     for i in range(n):
-        # print(arr_copy[i])
-        answer = min(answer, sum(arr_copy[i]))
+        answer = min(answer, sum(arr_copy[i])) # 배열의 최솟값
 print(answer)
