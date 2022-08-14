84b7542816c4b369c61fad08f7dab04495e46024
정석희 <86893209+seokhee516@users.noreply.github.com>
2022-08-14 21:16:30 +0900

    [Silver III] Title: 다이나믹 롤러, Time: 828 ms, Memory: 267316 KB -BaekjoonHub

--- 백준/Silver/17393. 다이나믹 롤러/다이나믹 롤러.py
+++ 백준/Silver/17393. 다이나믹 롤러/다이나믹 롤러.py
@@ -4,18 +4,16 @@ n = int(input())
 A = list(map(int, input().split())) # 잉크지수
 B = list(map(int, input().split())) # 점도지수
 ans = []
-for i in range(n):
-    s = 0
+for i in range(n): # 잉크지수 하나씩 확인
+    s = i
     e = n-1
     res = 0
     while s <= e:
         m = (s + e) // 2
-        if res == m:
-            break
-        elif A[i] < B[m]:
-            e = m - 1
-        else:
-            s = m + 1
-            res = m
-    ans.append(res-i)
+        if A[i] < B[m]: # 점도지수가 잉크지수보다 크다면
+            e = m - 1 # 더 작은 범위 확인
+        else: # 점도지수가 잉크지수보다 작거나 같다면
+            s = m + 1 # 더 큰 범위 확인
+            res = m # 결과값 업데이트
+    ans.append(res-i) # 현재 위치부터 점도지수가 잉크지수보다 작거나 같은 칸까지 개수
 print(*ans)
