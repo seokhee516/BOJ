c4999cba74b31eeee4d6b6555037df2a634fb87b
정석희 <86893209+seokhee516@users.noreply.github.com>
2022-08-20 13:26:50 +0900

    [level 2] Title: N-Queen, Time: 9747.34 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12952. N－Queen/N－Queen.py
@@ -0,0 +1,20 @@
+answer = 0
+def solution(n):
+    global answer
+    row = [0] * n
+    def is_promising(x):
+        for i in range(x):
+            if row[x] == row[i] or abs(row[x]-row[i]) == abs(x-i):
+                return False
+        return True
+    def n_queens(x):
+        global answer
+        if x == n:
+            answer += 1
+        else:
+            for i in range(n):
+                row[x] = i
+                if is_promising(x):
+                    n_queens(x+1)
+    n_queens(0)
+    return answer
