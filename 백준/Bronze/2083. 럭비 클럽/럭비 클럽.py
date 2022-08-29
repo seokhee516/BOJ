5f22824cfe485db2367cbb588ad1793b7dd747b8
정석희 <86893209+seokhee516@users.noreply.github.com>
2022-08-30 02:38:08 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.py
@@ -0,0 +1,11 @@
+name,age,weight='',0,0
+while True:
+    name,age,weight=input().split()
+    age=int(age)
+    weight=int(weight)
+    if name=='#' and age==0 and weight==0:
+        break
+    if age>17 or weight>=80:
+        print(name+' '+'Senior')
+    else:
+        print(name+' '+'Junior')
