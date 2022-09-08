c899f3756a343dead6afc92dc83247aa3042b7c3
정석희 <86893209+seokhee516@users.noreply.github.com>
2022-09-08 18:43:28 +0900

    Update 교점에 별 만들기.py

--- 프로그래머스/lv2/87377. 교점에 별 만들기/교점에 별 만들기.py
+++ 프로그래머스/lv2/87377. 교점에 별 만들기/교점에 별 만들기.py
@@ -1,6 +1,6 @@
-# 2차원 평면상의 좌표를 2차원 배열의 좌표로 찍어내려면 
+# 2차원 평면상의 좌표를 2차원 배열의 좌표로 찍는 방법 잘 기억해 두자
 # x, y = abs(max_y-y) , abs(min_x-x)
-# from itertools import combinations
+# from itertools import combinations # 콤비네이션으로 만드는것 보다 케이스가 클 때는 for문이 좀 더 빠르다
 def solution(line):
     # max_x, max_y = 0, 0
     # min_x, min_y = 1e9, 1e9
@@ -19,16 +19,16 @@ def solution(line):
                 x, y = int(x), int(y)
                 if (x,y) not in star:
                     star.append((x,y))
-                    # max_x = max(x, max_x)
+                    # max_x = max(x, max_x) # 이렇게 구하면 오류 날 수 있으니, 마지막에 구하자
                     # max_y = max(y, max_y)
                     # min_x = min(x, min_x)
                     # min_y = min(y, min_y)
     max_x, min_x = max(star)[0], min(star)[0]
     max_y, min_y = max(star, key=lambda x:x[1])[1], min(star, key=lambda x:x[1])[1]
-    ans = [['.']*(abs(max_x-min_x)+1) for _ in range(abs(max_y-min_y)+1)]
+    ans = [['.']*(abs(max_x-min_x)+1) for _ in range(abs(max_y-min_y)+1)] # 요기
     for x,y in star:
-        x, y = abs(max_y-y) , abs(min_x-x)
+        x, y = abs(max_y-y) , abs(min_x-x) # 요기
         ans[x][y] = "*"
     for i,v in enumerate(ans):
-        ans[i] = ''.join(v)
+        ans[i] = ''.join(v) # 2차원 배열에서 리스트 문자로 합칠 때는 index랑 value 뽑아서 넣기
     return ans
