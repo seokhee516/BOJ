62ccbc8c438b5adb55095bf096ab15cfbf6abe08
정석희 <86893209+seokhee516@users.noreply.github.com>
2022-09-11 11:49:06 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 424 ms, Memory: 134108 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,17 +1,19 @@
 import sys
 input = sys.stdin.readline
 n, m = map(int, input().split())
-graph = [[0] * n]
-for _ in range(n):
-    lst = list(map(int, input().split()))
-    lst.insert(0,0)
-    for i in range(1, n+1):
-        lst[i] = lst[i] + lst[i-1] # 누적합 구하기
-    graph.append(lst)
-
+dp = [[0 for _ in range(n+1)] for _ in range(n+1)]
+arr = [list(map(int, input().split())) for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        dp[i+1][j+1] = (dp[i][j+1] + dp[i+1][j] - dp[i][j]) + arr[i][j]
+'''
+print(dp)
+[[0, 0, 0, 0, 0], 
+[0, 1, 3, 6, 10], 
+[0, 3, 8, 15, 24], 
+[0, 6, 15, 27, 42], 
+[0, 10, 24, 42, 64]]
+'''
 for _ in range(m):
     x1, y1, x2, y2 = map(int, input().split())
-    ans = 0
-    for i in range(x1,x2+1):
-        ans += (graph[i][y2]-graph[i][y1-1]) # 구간합 구하기
-    print(ans)
+    print(dp[x2][y2]-(dp[x1-1][y2] + dp[x2][y1-1]-dp[x1-1][y1-1]))
