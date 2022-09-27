e4e9bd6422271813aba02e13b5438def5eb84ee9
정석희 <86893209+seokhee516@users.noreply.github.com>
2022-09-28 00:49:16 +0900

    Time: 459 ms (9.33%), Space: 15.9 MB (15.44%) - LeetHub

--- 455-assign-cookies/455-assign-cookies.py
+++ 455-assign-cookies/455-assign-cookies.py
@@ -1,15 +1,10 @@
-class Solution:
-    def findContentChildren(self, g: List[int], s: List[int]) -> int:
-        ans = 0
-        s.sort(reverse=True)
-        g.sort(reverse=True)
-
-        while g and s:
-            while s and s[-1] < g[-1]:
-                s.pop()
-            if s:
-                if s[-1]>=g[-1]:
-                    ans += 1
-                s.pop()
-                g.pop()
-        return ans
+class Solution(object):
+    def findContentChildren(self, g, s):
+        g.sort()
+        s.sort()
+        i, j = 0, 0
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                i += 1
+            j += 1
+        return i
