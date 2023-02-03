4f6ef26ecf5593c31b0cd23216091bca78e1af33
정석희 <86893209+seokhee516@users.noreply.github.com>
2023-02-03 21:38:18 +0900

    [Silver I] Title: 본대 산책, Time: 156 ms, Memory: 115416 KB -BaekjoonHub

--- 백준/Silver/12849. 본대 산책/본대 산책.py
+++ 백준/Silver/12849. 본대 산책/본대 산책.py
@@ -19,6 +19,8 @@ def nxt(state):
     tmp[5] = state[3] + state[4] + state[6]
     tmp[6] = state[5] + state[7]
     tmp[7] = state[4] + state[6]
+    for i in range(8):
+        tmp[i] %= 1000000007
     return tmp
 for i in range(int(input())):
     DP = nxt(DP)
