f36fe3bd82578a0011865264ade95c352e37c6c4
정석희 <86893209+seokhee516@users.noreply.github.com>
2023-05-12 01:27:23 +0900

    [unrated] Title: 당구 연습, Time: 2.14 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/169198. 당구 연습/당구 연습.py
+++ 프로그래머스/unrated/169198. 당구 연습/당구 연습.py
@@ -1,14 +1,3 @@
-def solution(m, n, x, y, balls):
+def solution(m, n, startX, startY, balls):
     answer = []
-    for a, b in balls:
-        tmp = []
-        if x != a or y > b:
-            tmp.append(abs(x-a)**2 + abs(2*n-y-b)**2)
-        if x != a or y < b:
-            tmp.append(abs(x-a)**2 + abs(y+b)**2)
-        if y != b or x < a:
-            tmp.append(abs(x+a)**2 + abs(y-b)**2)
-        if y != b or x > a:
-            tmp.append(abs(2*m-x-a)**2 + abs(y-b)**2)
-        answer.append(min(tmp))
     return answer
