51e53619d78d7ecba8a30ede3784281cf6396ca4
정석희 <86893209+seokhee516@users.noreply.github.com>
2023-05-19 16:25:58 +0900

    [level 4] Title: 가사 검색, Time: 4121.10 ms, Memory: 672 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/60060. 가사 검색/가사 검색.py
@@ -0,0 +1,51 @@
+import sys
+sys.setrecursionlimit(100001)
+def solution(words, queries):
+    answer = []
+    rev_words, counted = [], []
+    for w in words:
+        rev_words.append(w[::-1])
+        counted.append(len(w))
+    trie = make_trie({},words)
+    rev_trie = make_trie({}, rev_words)
+    # print(trie)
+    for query in queries:
+        if query[0] == '?' and query[-1] == '?':
+            answer.append(counted.count(len(query)))
+        elif query[0] == '?':
+            answer.append(search_trie(rev_trie, query[::-1],len(query)))
+        elif query[-1]=='?':
+            answer.append(search_trie(trie, query,len(query)))
+    return answer
+
+def make_trie(trie, words):
+    for word in words:
+        cur = trie
+        l = len(word)
+        for w in word:
+            if w in cur:
+                cur = cur[w]
+                cur['!'].append(l)
+            else:
+                cur[w] = {}
+                cur = cur[w]
+                cur['!'] = [l]
+    return trie
+
+def search_trie(trie, query,length):
+    count = 0
+    if query[0] == '?':
+        return trie['!'].count(length)
+    elif query[0] in trie:
+        count += search_trie(trie[query[0]], query[1:],length)
+    return count
+# def make_trie(dic, words):
+#     for word in words:
+#         current_dict = dic
+#         for letter in word:
+#             current_dict.setdefault(letter, [0, {}])
+#             current_dict[letter][0] +=1              
+#             current_dict = current_dict[letter][1]
+            
+#     return dic
+            
