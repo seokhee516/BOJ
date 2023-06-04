36eed46b377f873adc411f3f516cce245fd1bc24
정석희 <86893209+seokhee516@users.noreply.github.com>
2023-06-04 13:56:59 +0900

    [Gold II] Title: 친구 네트워크, Time: 716 ms, Memory: 197900 KB -BaekjoonHub

--- 백준/Gold/4195. 친구 네트워크/친구 네트워크.py
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.py
@@ -4,29 +4,26 @@ input = stdin.readline
 
 def find(x):
     if parent[x] != x:
-        parent[x] = find(parent[x])
+        parent[x] =  find(parent[x])
     return parent[x]
+def union(x,y):
+    x = find(x)
+    y = find(y)
+    if x != y:
+        parent[y] = x
+        number[x] += number[y]
+    print(number[x])
 
-
-def union(a, b):
-    a = find(a)
-    b = find(b)
-
-    if a != b:
-        parent[b] = a
-        number[a] += number[b]
-    print(number[a])
-
-
-for _ in range(int(input())):
-    num = int(input())
+t = int(input())
+for _ in range(t):
+    k = int(input())
     parent, number = {}, {}
-    for i in range(num):
-        a, b = input().split()
+    for _ in range(k):
+        a, b = input().strip().split()
         if a not in parent:
             parent[a] = a
             number[a] = 1
         if b not in parent:
-            parent[b] =b
+            parent[b] = b
             number[b] = 1
         union(a, b)
